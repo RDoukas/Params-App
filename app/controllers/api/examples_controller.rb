e2adc05c8cab6c1256cc134187bd594746f3df50
Becca <rrdoukas@gmail.com>
2020-06-01 13:48:29 -0500

    updates

---
+++ app/controllers/api/examples_controller.rb
@@ -0,0 +1,51 @@
+class Api::ExamplesController < ApplicationController
+
+  def query_params_action
+    @message = "The message is #{params['message']}"
+    @second_message = "The second message is #{params['second_message']}"
+    render "query_params.json.jb"
+  end
+
+  def query_name_action
+    name = params["name"].upcase
+    @message = "You name is #{name}"
+    if name.starts_with?("A")
+      @message = "Hey you name starts with an A, #{name}!"
+    end
+    render "query_name.json.jb"
+  end
+
+  def query_guess_action
+    @user_guess = params["guess"].to_i
+    winning_number = 36
+    if @user_guess > winning_number
+      @message = "Guess lower!"
+    elsif @user_guess < winning_number
+      @message = "Guess higher!"
+    else
+      @message = "You win!"
+    end
+    render "query_guess.json.jb"
+  end
+
+  def segment_params_action
+    @message = "The url segment is #{params[:wildcard]}"
+    render "segment_params.json.jb"
+  end
+
+  def body_params_action
+    @message = "The secret info is #{params[:secret_info]}"
+    render body_params.json.jb
+  end
+
+  def body_credentials_action 
+    @message = "Invalid credentials."
+    if params[:user_name].downcase == "hugh" && params[:password].downcase == "swordfish"
+      @message = "Valid credentials."
+    end 
+
+    render "body_guess.json.jb"
+  end
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # EXAMPLE HTML ROUTE
+  # get "/photos" => "photos#index"
+
+  # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  namespace :api do
+    get "/query_params_url" => "examples#query_params_action"
+    get "/query_name_url" => "examples#query_name_action"
+    get "/query_guess_url" => "examples#query_guess_action"
+
+    get "/segment_params_url/:wildcard" => "examples#segment_params_action"
+    get "/segment_guess_url/:guess" => "examples#query_guess_action"
+
+    post "/body_params_url" => "examples#body_params_action"
+    post "/body_guess_url" => "examples#query_guess_action"
+    post "/boddy_credentials_url" => "examples#body_credentials_action"
+  end
+end
